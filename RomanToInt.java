defafe1b9ea7e0955d52008e95c15f4d282284ae
Pathan Esuf khan <124810234+Esuf2@users.noreply.github.com>
2023-10-18 21:39:51 +0530

    Add files via upload

---
+++ RomanToInt.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RomanToInt {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0)
+            return -1;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int len = s.length(), result = map.get(s.charAt(len - 1));
+        for (int i = len - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1)))
+                result += map.get(s.charAt(i));
+            else
+                result -= map.get(s.charAt(i));
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        romanToInt(str);
+    }
+}
